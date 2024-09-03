9477301c9a56902e45fd63363046b1f0126d909d
hexagon66613 <reynaldo.oktavianus@seven-retail.com>
2024-09-03 10:37:23 +0700

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const midtransClient = require('midtrans-client');
+const app = express();
+
+app.use(bodyParser.json());
+
+const midtrans = new midtransClient.Snap({
+  isProduction: false, // Set to true for production
+  serverKey: 'YOUR_SERVER_KEY',
+});
+
+app.post('/create_transaction', (req, res) => {
+  const orderDetails = req.body;
+
+  midtrans.createTransaction(orderDetails)
+    .then(transaction => {
+      res.json({ token: transaction.token });
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
